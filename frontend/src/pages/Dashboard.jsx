5c7b69767bacc2ece2f349dc8b913288a193f740
Sourabh_Bakshi <bakshisourabh1l@gmail.com>
2025-04-22 20:42:53 +0530

    updated logout route

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,38 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Dashboard() {
+  const [loading, setloading] = useState(false);
+
+  const handleclick = async() => {
+    
+    try {
+      setloading(true);
+      const logout = await logoutUser();
+      
+      if(logout.success)
+      {
+        setloading(false);
+        window.location.href = '/';      
+      }
+      else
+      {
+        setloading(false);
+        alert('Logout Failed',logout.message);       
+      }
+
+    } catch (error) {
+      setloading(false);
+      console.log(error.message);
+      alert('Logout Error',error.message);
+      window.location.href = '/';
+      
+    }
+    
+  }
   
   return (
     <>
-    <div className="logout">Logout</div>
+    <div className="logout" onClick={handleclick}>Logout</div>
     <div>Dashboard</div>
     </>
   )
